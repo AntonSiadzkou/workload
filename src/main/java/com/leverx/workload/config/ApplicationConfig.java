1d28f1b05d7ad613bdfbf1a472c764ff211b0e98
Anton Siadzkou <anton.siadzkou@leverx.com>
2021-12-14 10:50:10 +0300

    m0-Basic_app_skeleton-configure_DB_connection_in_the_web_app:configured dataSource for postgresql

--- src/main/java/com/leverx/workload/config/ApplicationConfig.java
+++ src/main/java/com/leverx/workload/config/ApplicationConfig.java
@@ -1,11 +1,65 @@
 package com.leverx.workload.config;
 
+import java.util.Properties;
+import javax.sql.DataSource;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
 @EnableWebMvc
+@EnableTransactionManagement
+@Slf4j
 @ComponentScan(basePackages = "com.leverx")
+@PropertySource("classpath:app.properties")
 public class ApplicationConfig {
+
+  @Value("${jdbc.driverClass}")
+  private String driverClass;
+
+  @Value("${jdbc.url}")
+  private String jdbcUrl;
+
+  @Value("${jdbc.username}")
+  private String jdbcUsername;
+
+  @Value("${jdbc.password}")
+  private String jdbcPassword;
+
+  @Bean
+  public DataSource dataSource() {
+    DriverManagerDataSource dataSource = new DriverManagerDataSource();
+    dataSource.setDriverClassName(driverClass);
+    dataSource.setUrl(jdbcUrl);
+    dataSource.setUsername(jdbcUsername);
+    dataSource.setPassword(jdbcPassword);
+    return dataSource;
+  }
+
+  @Bean
+  public LocalSessionFactoryBean sessionFactory() {
+    LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
+    sessionFactory.setDataSource(dataSource());
+    sessionFactory.setPackagesToScan("com.leverx.entity");
+    Properties hibernateProps = new Properties();
+    hibernateProps.setProperty("hibernate.dialect", "org.hibernate.dialect.PostgreSQL10Dialect");
+    hibernateProps.setProperty("hibernate.show_sql", "true");
+    sessionFactory.setHibernateProperties(hibernateProps);
+    return sessionFactory;
+  }
+
+  @Bean
+  public HibernateTransactionManager transactionManager() {
+    HibernateTransactionManager transactionManager = new HibernateTransactionManager();
+    transactionManager.setSessionFactory(sessionFactory().getObject());
+    return transactionManager;
+  }
 }
